334efd7ae424e2a972270c4ed66dad68ab745e1b
zk <liuzq7@gmail.com>
2020-07-02 22:39:23 +0800

    fix version autogen

---
+++ version.py
@@ -0,0 +1,29 @@
+#coding: utf-8
+from pathlib import Path
+import subprocess
+import sys
+
+W  = '\033[0m'  # white (normal)
+R  = '\033[31m' # red
+G  = '\033[32m' # green
+
+head = subprocess.check_output(["git", "rev-parse" ,"HEAD"])
+current_commit = head.strip().decode('utf-8')
+
+versionfile = Path("./version")
+
+lines= versionfile.read_text().split("\n")
+
+version = lines[0]
+commit = lines[1]
+[mainv,modulev,minorv] = version.split(".")
+if commit != current_commit:
+    minorv=1+int(minorv)
+    print("bump minor version")
+else:
+    print("No git update ,no version change! bye!")
+    sys.exit(0)
+
+newversion =f"{mainv}.{modulev}.{minorv}"
+versionfile.write_text(newversion+"\n"+current_commit)
+print(G + f"{version} -> {newversion}"+ W)
